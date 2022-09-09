dbd6e76feda207d30988471561ab931c49d780b7
Ashok G.C <78782373+Ashok-gc@users.noreply.github.com>
2022-09-09 19:50:54 +0545

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 
 function App() {
   return (
     <>
-    <Navbar title="HelloTX" AboutText="About Us" Contact="Contact Us"/>
+    {/* <Navbar title="HelloTX" aboutText="About Us" Contact="Contact Us"/> */}
+    <Navbar title="HelloTX"/>
+    <div className="container my-5">
+    <TextForm heading="Enter the text to analyze below"/>
+    </div>
     </>
   );
 }
